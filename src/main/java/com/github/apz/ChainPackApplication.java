b4a3a736c9cae7f057642c29a1a345431e919cd8
A-pZ <g.alpha.pz@gmail.com>
2019-09-26 11:18:00 +0900

    warビルド設定

--- src/main/java/com/github/apz/ChainPackApplication.java
+++ src/main/java/com/github/apz/ChainPackApplication.java
@@ -2,9 +2,10 @@ package com.github.apz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChainPackApplication {
+public class ChainPackApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChainPackApplication.class, args);
