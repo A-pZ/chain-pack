b2bb9dfc245a77818a8d98aa854b5b8fae635f3c
A-pZ <g.alpha.pz@gmail.com>
2020-01-05 11:19:05 +0900

    Flyway 定義追加

--- src/main/resources/db/migration/V5_20190914_5__create_item.sql
+++ src/main/resources/db/migration/V5_20190914_5__create_item.sql
@@ -1,5 +1,5 @@
 CREATE TABLE test.item (
-	item_id INT NULL AUTO_INCREMENT COMMENT '商品id',
+	item_id INT NOT NULL AUTO_INCREMENT COMMENT '商品id',
 	name varchar(256) NULL COMMENT '商品名',
 	insert_time DATETIME NULL COMMENT '登録時間',
 	update_time DATETIME NULL COMMENT '更新時間',
